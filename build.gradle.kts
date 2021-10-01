0a2bed90885f3af8b604dd6fb1eb39bf58c93f5e
David Rawson <drawers@users.noreply.github.com>
2021-10-02 11:12:56 +1300

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.4.32"
+    kotlin("jvm") version "1.5.31"
 }
 
 group = "org.example"
@@ -11,11 +11,11 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-    implementation("com.squareup:kotlinpoet:1.8.0")
-    implementation("com.google.devtools.ksp:symbol-processing-api:1.5.10-1.0.0-beta01")
-    testImplementation("junit:junit:4.12")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+    implementation("com.squareup:kotlinpoet:1.10.1")
+    implementation("com.google.devtools.ksp:symbol-processing-api:1.5.31-1.0.0")
+    testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.4.32")
-    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.2")
-    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.4.2")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.4")
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.4.4")
 }
